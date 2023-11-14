8613943adfb704502ecd3a65d2dc43061e78a0e4
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-11-14 15:17:16 +0200

    create config class

--- rmbg_cli.py
+++ rmbg_cli.py
@@ -1,4 +1,28 @@
 import argparse
+import os
+import tomllib
+from pathlib import Path
+
+
+class Config:
+    @staticmethod
+    def _get_config_path() -> Path:
+        config_home = os.getenv("XDG_CONFIG_HOME")
+        path = (
+            Path(config_home) / "rmbg.toml"
+            if config_home
+            else Path.home() / ".rmbg.toml"
+        )
+        if not path.exists():
+            path.open("x").close()
+        return path
+
+    @staticmethod
+    def _get_config() -> dict:
+        path = Config._get_config_path()
+        with path.open("rb") as c:
+            data = tomllib.load(c)
+        return data
 
 
 def create_parser():
